6af891c3e0dccff3f4ccbe66b5dc9197d5244943
Jankees <mail@jankeesvw.com>
2019-07-11 21:22:18 +0200

    Only send unseen items

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
   def new_items(user_id)
     @user = User.find(user_id)
-    @feed_items = @user.feed_items.includes(:feed)
-    @feed_items.update_all(sent_at: Time.zone.now)
-
-    mail to: @user.email
+    @feed_items = @user.feed_items.unseen_items.includes(:feed)
+    if @feed_items.any?
+      mail(to: @user.email)
+      @feed_items.update_all(sent_at: Time.zone.now)
+    end
   end
 end

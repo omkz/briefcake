7150c183c30cebc9250a597465fcb9a155bb6315
Jankees <mail@jankeesvw.com>
2019-07-11 21:26:03 +0200

    Add decent subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,8 @@ class UserMailer < ApplicationMailer
     @user = User.find(user_id)
     @feed_items = @user.feed_items.unseen_items.includes(:feed)
     if @feed_items.any?
-      mail(to: @user.email)
+      date = I18n.l(Time.zone.today.to_date)
+      mail(to: @user.email, subject: "RSSMailer – #{@feed_items.count} new items – #{date}")
       @feed_items.update_all(sent_at: Time.zone.now)
     end
   end

0f8c8cf0c5b05ca9cd396e48217acc6dec2df758
Jankees <mail@jankeesvw.com>
2019-07-13 20:46:47 +0200

    Fix Email job

--- app/jobs/email_users_job.rb
+++ app/jobs/email_users_job.rb
@@ -2,6 +2,6 @@ class EmailUsersJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    User.find_each { |user| UserMailer.new_items(user).deliver }
+    User.find_each { |user| UserMailer.new_items(user.id).deliver }
   end
 end

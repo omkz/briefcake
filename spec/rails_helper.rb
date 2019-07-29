61dcdd5310a8962a7ea8f27e0d04ec68f86245a7
Jankees <mail@jankeesvw.com>
2019-07-29 21:23:27 +0200

    Allow connections in some specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.ignore_localhost = true
+  config.allow_http_connections_when_no_cassette = true
 end
 
 RSpec.configure do |config|

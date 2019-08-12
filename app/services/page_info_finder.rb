96999046bf4734f02fe890c8aca527e8494425ef
Jankees <mail@jankeesvw.com>
2019-08-12 21:21:46 +0200

    Return instagram url as feed_url

--- app/services/page_info_finder.rb
+++ app/services/page_info_finder.rb
@@ -25,6 +25,7 @@ class PageInfoFinder
 
   def feed_url
     return @url if is_rss_feed?
+    return @url if Feed.new(feed_url: @url).is_instagram?
 
     feed_url = @document.css("link[rel=alternate][type*=xml]")[0]["href"]
 

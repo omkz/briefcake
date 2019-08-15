7b9483cacc3f12396f6efc9342c21782561dd2d4
Jankees <mail@jankeesvw.com>
2019-08-15 20:13:24 +0200

    Remove slug columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_154144) do
+ActiveRecord::Schema.define(version: 2019_08_15_181136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,12 +70,10 @@ ActiveRecord::Schema.define(version: 2019_08_13_154144) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "slug"
     t.string "feed_url"
     t.datetime "publish_date_last_sent_item"
     t.integer "truncation"
     t.text "fetch_error"
-    t.index ["slug"], name: "index_feeds_on_slug", unique: true
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 

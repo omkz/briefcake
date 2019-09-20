b3710c2510325aa876c93b6222cd8b0efb45d404
Jankees <mail@jankeesvw.com>
2019-09-20 21:08:32 +0200

    Only send one email per day

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe UserMailer, type: :mailer do
+  it "does only send one email per day" do
+    user = create(:user)
+    feed = create(:feed, user: user)
+
+    Timecop.travel(Time.local(2019, 9, 20, 9, 0))
+
+    expect {
+      feed_item = build(:feed_item, feed: feed, publish_date: 1.hour.from_now)
+      allow_any_instance_of(Feed).to receive(:items!).and_return([feed_item])
+      UserMailer.new_items(user.id).deliver_now
+    }.to change(ActionMailer::Base.deliveries, :count).by(1)
+
+    Timecop.travel(1.hour)
+
+    expect {
+      feed_item = build(:feed_item, feed: feed, publish_date: 1.hour.from_now)
+      allow_any_instance_of(Feed).to receive(:items!).and_return([feed_item])
+      UserMailer.new_items(user.id).deliver_now
+    }.to_not change(ActionMailer::Base.deliveries, :count)
+
+    Timecop.travel(3.days)
+
+    expect {
+      feed_item = build(:feed_item, feed: feed, publish_date: 1.hour.from_now)
+      allow_any_instance_of(Feed).to receive(:items!).and_return([feed_item])
+      UserMailer.new_items(user.id).deliver_now
+    }.to change(ActionMailer::Base.deliveries, :count).by(1)
+  end
+end

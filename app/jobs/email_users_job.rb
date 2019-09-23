fd5641f7dba9499ead1189cb56d89351fe8c57d3
Jankees <mail@jankeesvw.com>
2019-09-23 16:18:31 +0200

    Work with time zones

--- app/jobs/email_users_job.rb
+++ app/jobs/email_users_job.rb
@@ -3,8 +3,13 @@ class EmailUsersJob < ApplicationJob
 
   def perform(*args)
     User.all.who_get_emails.find_each do |user|
-      midnight = Time.zone.now.at_midnight
-      run_at = midnight + (user.send_email_at.presence || 6.hours)
+      run_at = if user.send_email_at.present?
+                 result = user.send_email_at.in_time_zone(user.time_zone).utc
+                 result.to_date == Date.yesterday ? result + 24.hours : result
+               else
+                 Time.zone.now.at_midnight + 6.hours
+               end
+
       UserMailer.delay(queue: "new_items", priority: 10, run_at: run_at).new_items(user.id)
     end
   end
--- spec/jobs/email_users_job_spec.rb
+++ spec/jobs/email_users_job_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe EmailUsersJob do
   it "enqueues a mailer at the right time" do
-    create(:user, send_email_at: 9.hours)
+    create(:user, send_email_at: "9:00")
 
     EmailUsersJob.perform_now
 
@@ -23,6 +23,92 @@ describe EmailUsersJob do
     job = Delayed::Job.first
     expect(job.run_at).to eq Time.zone.today.midnight.advance(hours: 6)
     expect(job.priority).to eq 0
+  end
+
+  it "uses the users timezone to determine the time" do
+    Timecop.freeze Time.zone.parse("2019-09-23 00:00:00") do
+      create(:user, send_email_at: "9:00", time_zone: "Amsterdam")
+
+      EmailUsersJob.perform_now
+
+      expect(Delayed::Job.all).to have(1).job
+
+      job = Delayed::Job.first
+      expect(job.run_at).to eq Time.zone.today.midnight.advance(hours: 7)
+      expect(job.priority).to eq 0
+    end
+  end
+
+  describe "time_zones" do
+    it "uses the timezone to determine the time" do
+      Timecop.freeze Time.zone.parse("2019-09-23 00:00:00") do
+        create(:user, send_email_at: "9:00", time_zone: "Amsterdam")
+
+        EmailUsersJob.perform_now
+
+        expect(Delayed::Job.all).to have(1).job
+
+        job = Delayed::Job.first
+        expect(job.run_at.to_s).to eq "2019-09-23 07:00:00 UTC"
+        expect(job.priority).to eq 0
+      end
+    end
+
+    it "uses the timezone to determine the time (for Mountain Time (US & Canada))" do
+      Timecop.freeze Time.zone.parse("2019-09-23 00:00:00") do
+        create(:user, send_email_at: "9:00", time_zone: "Mountain Time (US & Canada)")
+
+        EmailUsersJob.perform_now
+
+        expect(Delayed::Job.all).to have(1).job
+
+        job = Delayed::Job.first
+        expect(job.run_at.to_s).to eq "2019-09-23 15:00:00 UTC"
+        expect(job.priority).to eq 0
+      end
+    end
+
+    it "uses the timezone to determine the time (for Sydney)" do
+      Timecop.freeze Time.zone.parse("2019-09-23 00:00:00") do
+        create(:user, send_email_at: "14:00", time_zone: "Sydney")
+
+        EmailUsersJob.perform_now
+
+        expect(Delayed::Job.all).to have(1).job
+
+        job = Delayed::Job.first
+        expect(job.run_at.to_s).to eq "2019-09-23 04:00:00 UTC"
+        expect(job.priority).to eq 0
+      end
+    end
+
+    it "uses the timezone to determine the time (for Sydney at midnight)" do
+      Timecop.freeze Time.zone.parse("2019-09-23 00:00:00") do
+        create(:user, send_email_at: "00:00", time_zone: "Sydney")
+
+        EmailUsersJob.perform_now
+
+        expect(Delayed::Job.all).to have(1).job
+
+        job = Delayed::Job.first
+        expect(job.run_at.to_s).to eq "2019-09-23 14:00:00 UTC"
+        expect(job.priority).to eq 0
+      end
+    end
+
+    it "uses the timezone to determine the time (for American Samoa at 16:00)" do
+      Timecop.freeze Time.zone.parse("2019-09-23 00:00:00") do
+        create(:user, send_email_at: "16:00", time_zone: "American Samoa")
+
+        EmailUsersJob.perform_now
+
+        expect(Delayed::Job.all).to have(1).job
+
+        job = Delayed::Job.first
+        expect(job.run_at.to_s).to eq "2019-09-23 03:00:00 UTC"
+        expect(job.priority).to eq 0
+      end
+    end
   end
 
   it "does not enqueue a job for someone who unsubscribed" do

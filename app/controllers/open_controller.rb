ff5694d3cc057fdca3a3becc2da9fb30a4d128f7
Jankees <mail@jankeesvw.com>
2019-09-23 13:14:08 +0200

    Make table wider

--- app/controllers/open_controller.rb
+++ app/controllers/open_controller.rb
@@ -4,7 +4,7 @@ class OpenController < ApplicationController
 
   def jobs
     @jobs = Delayed::Job.all
-    @todays_emails = SentEmail.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @todays_emails = SentEmail.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(:created_at)
     render layout: "clean"
   end
 end

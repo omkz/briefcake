e3c8eb34ba0e4a5e92f8c398c86e38c6e057a265
Henry Haller <henrybhaller@gmail.com>
2020-09-09 12:59:52 -0600

    improve testing, and the transition modal so that it redirects the user to the page that they expect to request

--- app/controllers/brief_cake_transition_controller.rb
+++ app/controllers/brief_cake_transition_controller.rb
@@ -1,6 +1,6 @@
 class BriefCakeTransitionController < ApplicationController
   def has_seen_briefcake
     session[:has_seen_briefcake] = true
-    redirect_to root_path
+    redirect_to params.require(:path), fallback_location: root_path
   end
 end
--- spec/controllers/brief_cake_transition_controller_spec.rb
+++ spec/controllers/brief_cake_transition_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe BriefCakeTransitionController, type: :controller do
 
   describe "GET #has_seen_briefcake" do
     it "returns http success" do
-      get :has_seen_briefcake
+      get :has_seen_briefcake, params: {path: "/"}
       expect(response).to have_http_status(:redirect)
     end
   end

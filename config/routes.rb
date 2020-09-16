299afd4284837f03b18863da9ed01b7c35e25851
Henry Haller <henrybhaller@gmail.com>
2020-09-16 14:39:18 -0600

    give exporting feeds a click through screen

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ Rails.application.routes.draw do
   get "/500", to: "errors#server_error"
   get "/422", to: "errors#unacceptable"
   get "/404", to: "errors#not_found"
+  get "/feeds/export", to: "feeds#export"
 
   ## more dynamic
   get "subscribe/show"

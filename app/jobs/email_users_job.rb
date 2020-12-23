8bd6506a1961d363faaf2bb9415ac1ab5962e831
Stanislav <git@skatkov.com>
2020-12-23 22:57:14 +0200

    more safety for EmailUsersJob

--- app/jobs/email_users_job.rb
+++ app/jobs/email_users_job.rb
@@ -2,16 +2,24 @@ class EmailUsersJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    User.all.who_get_emails.find_each do |user|
-      picked_specific_time = user.send_email_at.present?
-      run_at = if picked_specific_time
-                 result = user.send_email_at.in_time_zone(user.time_zone).utc
-                 result.to_date == Date.yesterday ? result + 24.hours : result
-               else
-                 Time.zone.now.at_midnight + 6.hours
-               end
+    User.who_get_emails.find_each do |user|
+      begin
+        picked_specific_time = user.send_email_at.present?
+        run_at = if picked_specific_time
+                   result = user.send_email_at.in_time_zone(user.time_zone).utc
+                   result.to_date == Date.yesterday ? result + 24.hours : result
+                 else
+                   Time.zone.now.at_midnight + 6.hours
+                 end
 
-      UserMailer.delay(queue: "new_items", priority: picked_specific_time ? 5 : 10, run_at: run_at).new_items(user.id)
+        UserMailer.delay(
+          queue: "new_items",
+          priority: picked_specific_time ? 5 : 10,
+          run_at: run_at)
+                  .new_items(user.id)
+      rescue => e
+        Honeybadger.notify(e)
+      end
     end
   end
 end

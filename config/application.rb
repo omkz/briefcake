bfc3862de957f3c6e61e2b5faa5ceb2c45048de5
Stanislav <git@skatkov.com>
2021-01-15 01:48:40 +0200

    turn on zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module Rssmailer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     config.application_naked_domain = "briefcake.com"
     config.application_name = "Briefcake"

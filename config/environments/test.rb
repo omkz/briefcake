1f4b284892fcde09648bbfc7adc43d8571a832f9
Stanislav (Stas) Katkov <sk@skylup.com>
2021-02-13 15:54:53 +0100

    Zeitwerk: check that app could be autoload (#224)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  config.eager_load = false
+  # Verify on CI that zeitwerk can autoload entire app properly
+  config.eager_load = ENV["CI"] ? true : false
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true

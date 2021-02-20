b023fecd3038cfa75fec0e644b475bd82bbdce48
Stanislav Katkov <sk@skylup.com>
2021-02-20 12:13:54 +0100

    don't preserve finished jobs

--- config/initializers/goob_job.rb
+++ config/initializers/goob_job.rb
@@ -1,4 +1,4 @@
-GoodJob.preserve_job_records = true
+GoodJob.preserve_job_records = false
 GoodJob.retry_on_unhandled_error = false
 GoodJob.on_thread_error = -> (exception) { Honeybadger.notify(exception) }
 

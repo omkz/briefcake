08a0e6215348dae0fb974aeee03ac974c14dc3a0
Stanislav Katkov <sk@skylup.com>
2021-09-13 20:12:58 +0200

    change asset_host to app.briefcake.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.middleware.use Rack::Deflater
 
-  config.application_domain = "https://briefcake.com"
+  config.application_domain = "https://app.briefcake.com"
   routes.default_url_options[:host] = config.application_domain
 
   config.action_mailer.default_url_options = { host: Rails.configuration.application_domain }
@@ -53,7 +53,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "https://briefcake.com"
+  config.action_controller.asset_host = "https://app.briefcake.com"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
